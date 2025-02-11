3377d7ae7343c1fb901fe67fad74028a3072b6b4
MD Kamrul Hasan Shahed <kh@powerledger.io>
2025-02-11 15:36:49 +0600

    Initial commit after generating project using start.spring.io

---
+++ src/main/java/com/practice/log_stream_poc/LogStreamPocApplication.java
@@ -0,0 +1,13 @@
+package com.practice.log_stream_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogStreamPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogStreamPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/log_stream_poc/LogStreamPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.log_stream_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogStreamPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
